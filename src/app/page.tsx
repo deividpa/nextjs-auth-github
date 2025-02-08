06b5f269854bb2592f57c7ac77f43b46c71c1657
David Pérez <deividpa022@gmail.com>
2025-02-08 13:14:56 -0500

    feat: Update Home component to use server-side authentication and display user information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
-"use client";
+"use server";
 
-import { login } from "@/lib/actions/auth";
+import { auth } from "@/auth";
+import { SignInButton } from "./components/sign-in-button";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await auth();
+  console.log(session)
+
+  if(session?.user) {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">
+          NextAuth v5 project using NextJS
+        </h1>
+        <p className="text-xl text-gray-700 mb-6">
+          You are signed in as {session.user.email}
+        </p>
+        { session.user.image &&
+          <Image 
+            src={session.user.image || "/default-image.png"}
+            alt={session.user.name || "User Image"}
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        }
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <p>You are not Signed In</p>
-      <button onClick={login}>Sign In With Github</button>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+      <p className="text-xl text-gray-700 mb-6">You are not Signed In</p>
+      <SignInButton />
     </div>
   );
 }
