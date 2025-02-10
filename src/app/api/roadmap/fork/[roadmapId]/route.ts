52542fea1838c950a3fee5c6fd87f30661b06121
David Pérez <deividpa022@gmail.com>
2025-02-10 00:43:39 -0500

    feat: Implement POST route for forking public roadmaps with authentication

---
+++ src/app/api/roadmap/fork/[roadmapId]/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { forkRoadmap } from "@/lib/actions/roadmap";
+import { prisma } from "@/prisma/prisma";
+import { auth } from "@/auth";
+
+export async function POST(
+  request: Request,
+  { params }: { params: { roadmapId: string } }
+) {
+  // Check if the user is authenticated
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  
+  const { roadmapId } = params;
+  
+  // Check if the roadmap exists and is public
+  const original = await prisma.roadmap.findUnique({
+    where: { id: roadmapId }
+  });
+  if (!original) {
+    return NextResponse.json({ message: "Original roadmap not found" }, { status: 404 });
+  }
+  if (!original.isPublic) {
+    return NextResponse.json({ message: "It's not possible to fork private roadmaps" }, { status: 403 });
+  }
+  
+  try {
+    const newRoadmap = await forkRoadmap(session.user.id, roadmapId);
+    return NextResponse.json(newRoadmap, { status: 201 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ message: "An unknown error occurred" }, { status: 500 });
+  }
+}
