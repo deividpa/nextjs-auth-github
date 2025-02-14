3d9ea21b0834d3b9ef542604ea0c34b8be115d5e
David Pérez <deividpa022@gmail.com>
2025-02-13 20:03:03 -0500

    feat: Enhance layout with footer and improved main content styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Providers } from "./providers";
 import { Header } from "./components/layout/Header";
+import { Footer } from "./components/layout/Foooter";
 import "./globals.css";
 
 export default function RootLayout({
@@ -10,11 +11,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="antialiased"
+        className="antialiased min-h-screen flex flex-col"
       >
         <Providers>
           <Header />
-          <main>{children}</main>
+          <main className="flex-grow bg-gradient-to-tl from-gray-100 to-gray-300">
+            <div className="max-w-7xl mx-auto w-full px-4 sm:px-6 lg:px-8 py-6">
+              {children}
+            </div>
+          </main>
+          <Footer />
         </Providers>
       </body>
     </html>
