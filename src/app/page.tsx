4256ce193d0ec6fb693c337d460d058ec588cef7
David Pérez <deividpa022@gmail.com>
2025-02-15 19:43:06 -0500

    feat: Refactor LandingPage to use client-side rendering and display last public roadmaps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,39 @@
-"use server";
+"use client";
 
-import { auth } from "@/auth";
-import Image from "next/image";
-import { redirect } from "next/navigation";
+import React from "react";
+import { usePublicRoadmaps } from "@/lib/queries/usePublicRoadmaps";
+import RoadmapCard from "@/app/components/roadmap/RoadmapCard";
+import SkeletonCard from "@/app/components/roadmap/SkeletonCard";
 
-export default async function LandingPage() {
-  const session = await auth();
-
-  if (session?.user) {
-    redirect("/dashboard");
-  }
+export default function LandingPage() {
+  const { data, isLoading, error } = usePublicRoadmaps(3);
 
   return (
-    <div className="flex h-full items-center justify-center p-4">
-      <div className="p-8 rounded-lg shadow-lg text-center space-y-6 bg-black/10">
-        <h1 className="text-2xl font-bold mb-2">Welcome to Roadmap Creator</h1>
-
-        <Image
-          src="/images/roadmap.jpg"
-          alt="Roadmap example"
-          width={300}
-          height={250}
-          className="rounded-lg shadow-2xl mx-auto my-4"
-        />
-
-        <p className="mb-6 text-md max-w-2xl text-center">
-          Generate your personalized roadmap for the day with the help of Gemini AI. Stay organized, set your goals, and track your progress!
+    <div className="flex flex-col items-center justify-center p-4 bg-cover bg-center">
+      <div className="p-6 rounded-lg shadow-lg text-center space-y-6 bg-secondary/90 backdrop-blur-sm w-full">
+        <h1 className="text-4xl font-bold mb-4 text-white">Welcome to Roadmap Creator</h1>
+        <p className="mb-4 text-lg text-center text-white">
+          Generate your personalized roadmap for the day. Stay organized, set your goals, and track your progress!
         </p>
+      </div>
+
+      <div className="w-full max-w-6xl mt-4">
+        <h2 className="text-2xl font-bold mb-4 text-primary">Last Public Roadmaps</h2>
+        {isLoading && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+            {Array.from({ length: 3 }).map((_, idx) => (
+              <SkeletonCard key={idx} />
+            ))}
+          </div>
+        )}
+        {error && <p className="p-4 text-center text-red-600">Error loading the roadmaps</p>}
+        {data && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+            {data.map((roadmap) => (
+              <RoadmapCard key={roadmap.id} roadmap={roadmap} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
